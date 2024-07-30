e26142d2745407e21baba35e01abc534875fed0f
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-07-30 15:29:38 +0200

    preparatory phase

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ console.log('JS OK')
 // 4. Ask the user for his or her favourite color
 // 5. Add up the strings to generate a password
 // 6. Insert the result into the DOM
+
+// PREPARATORY PHASE
+// 1. Retrieve the element of my interest from the DOM
+const resultElement = document.getElementById('pwdgen');
+console.log(resultElement);
+
+// 2. Ask the user for his or her name
