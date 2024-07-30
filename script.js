3d9083f8168ed50abce6baa19125cceb82c1cd9a
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-07-30 15:20:29 +0200

    added script comments

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('JS OK')
+
+// 1. Retrieve the element of my interest from the DOM
+// 2. Ask the user for his or her name
+// 3. Ask the user for his or her last name
+// 4. Ask the user for his or her favourite color
+// 5. Add up the strings to generate a password
+// 6. Insert the result into the DOM
